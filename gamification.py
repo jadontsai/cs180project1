e3d73cbcaf6b47cd3729ea2874ec70997fb4bf63
jadontsai <154614626+jadontsai@users.noreply.github.com>
2024-09-30 15:18:14 -0400

    Update gamification.py

--- gamification.py
+++ gamification.py
@@ -1,11 +1,11 @@
 def initialize():
-    '''Initializes the global variables needed for the simulation.
+    '''Initializes the global variables needed for the simulation. 
     Note: this function is incomplete, and you may want to modify it'''
     global star_time
-    star_time = 0
+    star_time = -1
     
     global star_time_2
-    star_time_2 = 0
+    star_time_2 = -1
     
     global star_time_delta
     star_time_delta = 0
@@ -14,7 +14,7 @@ def initialize():
     star_time_delta_2 = 0
     
     global star_time_3
-    star_time_3 = 0
+    star_time_3 = -1
 
     global star_time_delta_3
     star_time_delta_3 = 0
@@ -38,7 +38,7 @@ def initialize():
     cur_star = None
     cur_star_activity = None
     
-    bored_with_stars = None
+    bored_with_stars = False
     
     last_activity = None
     last_activity_duration = 0
@@ -104,35 +104,68 @@ def tiredness():
     else:
         return False
 
+def offer_star(activity):
+    global last_activity
+    global cur_star
+    global cur_star_activity
+    global cur_time
+    global star_time, star_time_2, star_time_3, star_time_delta, star_time_delta_2, star_time_delta_3, bored_with_stars
+
+    star_time_3 = star_time_2
+    star_time_2 = star_time
+    star_time = cur_time
+
+    if (star_time_3 - star_time < 120) and (star_time_3 != -1):
+        bored_with_stars = True
+
+
+
+    if activity == "running":
+        cur_star = "running"
+        cur_star_activity = "running"
+        return cur_star
+    elif activity == "textbooks":
+        cur_star = "textbooks"
+        cur_star_activity = "textbooks"
+        return cur_star
+
 
 def star_can_be_taken(activity):
     global last_activity
     global bored_with_stars
     global cur_time
     global star_time, star_time_2, star_time_3, star_time_delta, star_time_delta_2, star_time_delta_3, placeholder
+    if (cur_star_activity == activity) and (bored_with_stars == False):
+         return True
+    else:
+         return False
+    #if star is offered, (cur_star == cur_star_activity), bored of stars is false, and 
 
-    if star_time != 0 and star_time_3 == 0: # if this isn't the first star specifically second
-        star_time_2 = cur_time #say star2 offered at 20
-        star_time_delta = star_time_2 - star_time
-    elif star_time == 0:
-        star_time = cur_time
+def most_fun_activity_minute():
+    global cur_star_activity
+    global cur_star
+    global tired
+    #return activity with most hedons in one minute
+    # not tired running returns 2 hpm
+    # not tired textbook returns 1hpm
+    #resting returns 0 always
+    #tired turns negative
+    # if tired AND no star, return resting
+    # if tired and star for something, return the thing
+    # if not tired,  and no star return running
+    #if not tired, and star for something, return the thing
+    if cur_star == "running" and cur_star_activity == "running" and bored_with_stars == False:
+        return "running"
+        
+    elif cur_star == "textbooks" and cur_star_activity == "textbooks" and bored_with_stars == False:
+        return "textbooks"
+    
+    elif tiredness() == False: #not tired
 
-    if star_time_delta != 0 and star_time_2 == cur_time: #just in case
-        star_time_3 = cur_time
-        star_time_delta_2 = star_time_3 - star_time_2
-    if star_time != 0 and star_time_3 != 0:
-        star_time_3 = cur_time
-        star_time_delta_3 = star_time_3 - star_time_2
-    if star_time_delta + star_time_delta_2 + star_time_delta_3 < 120 and star_time_3 != star_time_2: #so that two zeros dont add up
-        bored_with_stars = True
-        return False
-    elif star_time_delta + star_time_delta_2 + star_time_delta_3 >= 120:
-        bored_with_stars = False
-        star_time_delta = 0
-        return True
-    elif star_time_3 == star_time_2:
-        bored_with_stars = False
-        return True
+        return "running"
+    elif tiredness() == True: # tired
+        return "resting"
+            
 
 def running_activity(activity, duration):
     global previous_activity_1
@@ -176,9 +209,9 @@ def perform_activity(activity, duration):
         if (tiredness() == True) and cur_star != "running":
                 # loses 2 hedons IF NO STAR and IF tired, loses 2 hedons per minute
                 cur_hedons -= 2 * duration 
-        if (tiredness() == False) and duration <= 10: #not tired
+        if (tiredness() == False) and (duration <= 10) and (cur_star != "running"): #not tired
                 cur_hedons += duration*2
-        if (tiredness() == False) and duration > 10: #not tired
+        if (tiredness() == False) and (duration > 10) and (cur_star != "running"): #not tired
                 cur_hedons += 20 - (duration-10)*2
         
         if (tiredness() == False) and duration > 10 and cur_star == "running": #not tired
@@ -236,51 +269,3 @@ def get_cur_hedons():
 def get_cur_health():
     global cur_health
     return(cur_health)
-    
-def offer_star(activity):
-    global last_activity
-    global cur_star
-    global cur_star_activity
-    if activity == "running" and star_can_be_taken(activity) == True:
-        cur_star = "running"
-        cur_star_activity = "running"
-        return cur_star
-    elif activity == "textbooks" and star_can_be_taken(activity) == True:
-        cur_star = "textbooks"
-        cur_star_activity = "textbooks"
-        return cur_star
-    else:
-        star_can_be_taken(activity)
-        return None
-
-def most_fun_activity_minute():
-    global cur_star_activity
-    global cur_star
-    global tired
-    #return activity with most hedons in one minute
-    # not tired running returns 2 hpm
-    # not tired textbook returns 1hpm
-    #resting returns 0 always
-    #tired turns negative
-    # if tired AND no star, return resting
-    # if tired and star for something, return the thing
-    # if not tired,  and no star return running
-    #if not tired, and star for something, return the thing
-    if cur_star == "running" and cur_star_activity == "running":
-        return "running"
-        
-    elif cur_star == "textbooks" and cur_star_activity == "textbooks":
-        return "textbooks"
-    
-    elif cur_star == None and tiredness() == False: #not tired
-
-        return "running"
-    elif cur_star == None and tiredness() == True: # tired
-        return "resting"
-            
-if __name__ == '__main__':
-    initialize()
-    perform_activity("textbooks", 14)
-    print(most_fun_activity_minute())
-    print(get_cur_health())
-    print(get_cur_hedons())
